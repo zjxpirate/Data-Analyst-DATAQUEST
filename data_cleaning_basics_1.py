2a1ad24c21aff2b4f683d0f5817268643da5f2a8
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2019-01-25 16:08:15 -0800

    01/25/2019

---
+++ data_cleaning_basics_1.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+import pandas as pd
+
+
+#laptops = pd.read_csv("laptops.csv", encoding="UTF-8")
+laptops = pd.read_csv("laptops.csv", encoding="Windows-1251")
+#laptops = pd.read_csv("laptops.csv", encoding="Latin-1")
+
+
+laptops.info()
+
+
+
+
+
+
+
