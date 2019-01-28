e97275cd9bc98afc4a183b73c1e2057419dda9d7
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2019-01-28 12:50:50 -0800

    01/28/2019

---
+++ Data_Cleaning_Basics/data_cleaning_basics_7.py
@@ -0,0 +1,162 @@
+
+
+
+
+import pandas as pd
+
+#laptops = pd.read_csv("laptops.csv", encoding="UTF-8")
+laptops = pd.read_csv("laptops.csv", encoding="Windows-1251")
+#laptops = pd.read_csv("laptops.csv", encoding="Latin-1")
+
+#laptops.info()
+
+# 1. Removing any whitespace from the start and end of the labels
+# 2. Replacing spaces with underscores and remove special characters - this will make things consistent,
+#    and also allows for the use of dot accessors for those who prefer that.
+# 3. Make all labels lowercase - this is good for consistency, and means you'll never have to remember
+#    what is capitalized and how.
+# 4. Shorten any long column names - this helps to keep your code easier to read, especially when you
+#    are using method chaining.
+
+#print(laptops.columns)
+
+laptops_test = laptops.copy()
+laptops_test.columns = ['A', 'B', 'C', 'D', 'E', 'G', 'F', 'H', 'I', 'J', 'K', 'L', 'M']
+#print(laptops_test.columns)
+
+def clean_col(col):
+    col = col.strip()
+    col = col.replace("(","")
+    col = col.replace(")","")
+    col = col.lower()
+    return col
+
+laptops.columns = [clean_col(c) for c in laptops.columns]
+
+def modify(l1):
+    l1 = l1.strip()
+    l1 = l1.replace("Operating System", "os")
+    l1 = l1.replace(" ", "_")
+    l1 = l1.replace("(", "")
+    l1 = l1.replace(")", "")
+    l1 = l1.lower()
+    return l1
+
+laptops.columns = [modify(i) for i in laptops.columns]
+#print(laptops.columns)
+
+#print("Type: ", laptops["screen_size"].dtype)
+#print(laptops["screen_size"].unique())
+
+laptops["screen_size"] = laptops["screen_size"].str.replace('"','')
+#print(laptops["screen_size"].unique())
+
+laptops["screen_size"] = laptops["screen_size"].astype(float)
+#print(laptops["screen_size"].dtype)
+#print(laptops["screen_size"].unique())
+
+laptops.rename({"screen_size": "screen_size_inches"}, axis=1, inplace=True)
+#print(laptops.dtypes)
+
+#print(laptops["ram"].unique())
+
+laptops["ram"] = laptops["ram"].str.replace('GB', '')
+
+laptops["ram"] = laptops["ram"].astype(int)
+
+laptops.rename({"ram": "ram_gb"}, axis=1, inplace=True)
+
+dtypes = laptops.dtypes
+
+
+#laptops["weight"] = (laptops["weight"].str.replace("kg","").astype(float))
+#print(laptops.loc[laptops["weight"].str.contains('s'), "weight"])
+
+#print(laptops["price_euros"].unique()[:5])
+#print(laptops["price_euros"].unique()[-5:])
+
+laptops["weight"] = laptops["weight"].str.replace("kgs", "").str.replace("kg", "")
+laptops["weight"] = laptops["weight"].astype(float)
+laptops.rename({"weight": "weight_kg"}, axis=1, inplace=True)
+
+laptops["price_euros"] = laptops["price_euros"].str.replace(",", ".")
+laptops["price_euros"] = laptops["price_euros"].astype(float)
+
+weight_describe = laptops["weight_kg"].describe()
+price_describe = laptops["price_euros"].describe()
+
+
+
+
+
+# 5 begins here:
+
+result = laptops["gpu"].head()
+
+result = laptops["gpu"].head().str.split()
+
+result = laptops["gpu"].head().str.split(n=1)
+
+result = laptops["gpu"].head().str.split(n=1, expand=True)
+
+result = laptops["gpu"].head().str.split(n=1, expand=True).iloc[:, 0]
+
+result = laptops["cpu"].head(10)
+
+laptops["cpu_manufacturer"] = (laptops["cpu"].str.split(n=1, expand=True).iloc[:,0])
+
+#print(laptops)
+
+
+
+
+
+# 6 begins here:
+
+result = laptops["screen"].unique().shape
+result = laptops["screen"].unique()[:10]
+result = laptops.loc[:9, "screen"].str.split(expand=True)
+
+result = laptops.loc[:9, "screen"].str.rsplit(n=1, expand=True)
+
+result = laptops["screen"].str.rsplit(n=1, expand=True)
+result.columns = ["A", "B"]
+result.loc[result["B"].isnull(), "B"] = result["A"]
+
+laptops["screen_resolution"] = result["B"]
+#print(laptops["screen_resolution"].unique())
+
+result = laptops["cpu"].unique()[:5]
+
+laptops["cpu_speed_ghz"] = laptops["cpu"].str.replace("GHz", "").str.rsplit(n=1, expand=True).iloc[:,1].astype(float)
+
+#print(laptops.loc[:10, "cpu_speed_ghz"])
+
+
+
+
+
+
+# 7 begins here:
+
+#print(laptops["operating_system"].value_counts())
+
+mapping_dict = {
+    'Android': 'Android',
+    'Chrome OS': 'Chrome OS',
+    'Linux': 'Linux',
+    'Mac OS': 'macOS',
+    'No OS': 'No OS',
+    'Windows': 'Windows',
+    'macOS': 'macOS'
+}
+
+laptops["operating_system"] = laptops["operating_system"].map(mapping_dict)
+
+print(laptops["operating_system"].unique())
+
+
+
+
+
+
