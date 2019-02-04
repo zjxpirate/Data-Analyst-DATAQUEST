f72e348ac73a7bb55fcca07f4021bb517e4b138e
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2019-02-04 14:41:39 -0800

    02/04/2019

---
+++ bar_plots_and_scatter_plots/bar_plots_and_scatter_plots_7.py
@@ -0,0 +1,100 @@
+
+
+# 2 begins here:
+import pandas as pd
+
+reviews = pd.read_csv("fandango_scores.csv")
+
+cols = ['FILM', 'RT_user_norm', 'Metacritic_user_nom', 'IMDB_norm', 'Fandango_Ratingvalue', 'Fandango_Stars']
+norm_reviews = reviews[cols]
+#print(norm_reviews[:1])
+
+
+
+
+
+# 4 begins here:
+
+import matplotlib.pyplot as plt
+from numpy import arange
+num_cols = ['RT_user_norm', 'Metacritic_user_nom', 'IMDB_norm', 'Fandango_Ratingvalue', 'Fandango_Stars']
+
+bar_heights = norm_reviews[num_cols].iloc[0].values
+bar_positions = arange(5) + 0.75
+
+# fig, ax = plt.subplots()
+#
+# ax.bar(bar_positions, bar_heights, 0.5)
+#
+# plt.show()
+
+
+
+
+
+
+
+# 5 begins here:
+
+num_cols = ['RT_user_norm', 'Metacritic_user_nom', 'IMDB_norm', 'Fandango_Ratingvalue', 'Fandango_Stars']
+bar_heights = norm_reviews[num_cols].iloc[0].values
+bar_positions = arange(5) + 0.75
+tick_positions = range(1,6)
+
+# fig, ax = plt.subplots()
+#
+# ax.bar(bar_positions, bar_heights, 0.5)
+# ax.set_xticks(tick_positions)
+# ax.set_xticklabels(num_cols, rotation=90)
+#
+# ax.set_xlabel('Rating Source')
+# ax.set_ylabel('Average Rating')
+# ax.set_title('Average User Rating For Avengers: Age of Ultron (2015)')
+# plt.show()
+
+
+
+
+
+
+# 6 begins here:
+
+import matplotlib.pyplot as plt
+from numpy import arange
+num_cols = ['RT_user_norm', 'Metacritic_user_nom', 'IMDB_norm', 'Fandango_Ratingvalue', 'Fandango_Stars']
+
+bar_widths = norm_reviews[num_cols].iloc[0].values
+bar_positions = arange(5) + 0.75
+tick_positions = range(1,6)
+
+
+
+# fig, ax = plt.subplots()
+# ax.barh(bar_positions, bar_widths, 0.5)
+#
+# ax.set_yticks(tick_positions)
+# ax.set_yticklabels(num_cols)
+# ax.set_ylabel('Rating Source')
+# ax.set_xlabel('Average Rating')
+# ax.set_title('Average User Rating For Avengers: Age of Ultron (2015)')
+# plt.show()
+
+
+
+
+
+
+# 7 begins here:
+
+fig, ax = plt.subplots()
+
+ax.scatter(norm_reviews['Fandango_Ratingvalue'], norm_reviews['RT_user_norm'])
+
+ax.set_xlabel('Fandango')
+
+ax.set_ylabel('Rotten Tomatoes')
+
+plt.show()
+
+
+
